6a91d471e480bea8677ad7bd7ef6fa67247df5f6
nazebzurati <nazeb04@gmail.com>
2020-01-14 23:21:07 +0800

    Add e-Solat parser

---
+++ lib/model/prayerTimeData.dart
@@ -0,0 +1,40 @@
+class PrayerTimeData {
+  // field
+  String hijri;
+  String date;
+  String day;
+  String imsak;
+  String fajr;
+  String syuruk;
+  String dhuhr;
+  String asr;
+  String maghrib;
+  String isha;
+  // method
+  PrayerTimeData(
+      {this.hijri,
+      this.date,
+      this.day,
+      this.imsak,
+      this.fajr,
+      this.syuruk,
+      this.dhuhr,
+      this.asr,
+      this.maghrib,
+      this.isha});
+  // factory
+  factory PrayerTimeData.fromJson(Map<String, dynamic> parsedJson) {
+    return PrayerTimeData(
+      hijri: parsedJson['hijri'],
+      date: parsedJson['date'],
+      day: parsedJson['day'],
+      imsak: parsedJson['imsak'],
+      fajr: parsedJson['fajr'],
+      syuruk: parsedJson['syuruk'],
+      dhuhr: parsedJson['dhuhr'],
+      asr: parsedJson['asr'],
+      maghrib: parsedJson['maghrib'],
+      isha: parsedJson['isha'],
+    );
+  }
+}
---
+++ lib/parse.dart
@@ -0,0 +1,12 @@
+import 'package:http/http.dart';
+
+class ESolatParser {
+  // field
+  String url =
+      "https://www.e-solat.gov.my/index.php?r=esolatApi/takwimsolat&period=year";
+  // method
+  Future<String> getZoneData(String zone) async {
+    var response = await post(url + "&zone=" + zone);
+    return response.body.toString();
+  }
+}
