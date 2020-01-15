147a97aa7e8202a83160adcc586540a2737f0920
nazebzurati <nazeb04@gmail.com>
2020-01-15 22:46:27 +0800

    Add database implementation

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:waktuku/model/prayerTimeData.dart';
+import 'package:waktuku/database/database_helper.dart';
+import 'package:waktuku/model/prayer_time.dart';
 import 'package:waktuku/parse.dart';
 
 void main() => runApp(MyApp());
@@ -37,19 +38,30 @@ class TestState extends State<Test> {
   Widget build(BuildContext context) {
     return FutureBuilder<String>(
       future: parser.getZoneData("SGR03"),
-      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+      builder: (BuildContext c1, AsyncSnapshot<String> s1) {
         // get current date
         var formatter = new DateFormat('dd-MMM-yyyy');
         String formatted = formatter.format(DateTime.now());
         // parse json
         List<PrayerTimeData> data =
-            (jsonDecode(snapshot.data)['prayerTime'] as List).map((i) {
+            (jsonDecode(s1.data)['prayerTime'] as List).map((i) {
           return PrayerTimeData.fromJson(i);
         }).toList();
         PrayerTimeData cur =
             data.singleWhere((i) => i.date.contains(formatted));
+        DatabaseHelper.instance.ptInsert("SGR03", cur);
         // return
-        return Text(cur.date + "; " + cur.hijri);
+        return FutureBuilder<int>(
+          future: DatabaseHelper.instance.ptInsert("SGR03", cur),
+          builder: (BuildContext c2, AsyncSnapshot<int> s2) {
+            return FutureBuilder<int>(
+              future: DatabaseHelper.instance.ptRowCount(),
+              builder: (BuildContext c3, AsyncSnapshot<int> s3) {
+                return Text(s3.data.toString());
+              },
+            );
+          },
+        );
       },
     );
   }
