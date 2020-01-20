2901f1d1c24fe2f2b3ce2ed88deb2555041553c7
nazebzurati <nazeb04@gmail.com>
2020-01-20 23:01:28 +0800

    Add fetching zone list

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'package:waktuku/logic/database.dart';
-import 'package:waktuku/model/prayer_time.dart';
 import 'package:waktuku/logic/controller.dart';
 
 void main() => runApp(MyApp());
@@ -33,35 +28,16 @@ class Test extends StatefulWidget {
 }
 
 class TestState extends State<Test> {
-  PrayerTimeController parser = PrayerTimeController();
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<String>(
-      future: parser.getZoneData("SGR03"),
-      builder: (BuildContext c1, AsyncSnapshot<String> s1) {
-        // get current date
-        var formatter = new DateFormat('dd-MMM-yyyy');
-        String formatted = formatter.format(DateTime.now());
-        // parse json
-        List<PrayerTimeData> data =
-            (jsonDecode(s1.data)['prayerTime'] as List).map((i) {
-          return PrayerTimeData.fromJson(i);
-        }).toList();
-        PrayerTimeData cur =
-            data.singleWhere((i) => i.date.contains(formatted));
-        DatabaseHelper.getInstance.ptInsert("SGR03", cur);
-        // return
-        return FutureBuilder<int>(
-          future: DatabaseHelper.getInstance.ptInsert("SGR03", cur),
-          builder: (BuildContext c2, AsyncSnapshot<int> s2) {
-            return FutureBuilder<int>(
-              future: DatabaseHelper.getInstance.ptRowCount(),
-              builder: (BuildContext c3, AsyncSnapshot<int> s3) {
-                return Text(s3.data.toString());
-              },
-            );
-          },
-        );
+    return FutureBuilder<List<String>>(
+      future: getZoneList(),
+      builder: (BuildContext c2, AsyncSnapshot<List<String>> s2) {
+        String s = "";
+        for (String item in s2.data) {
+          s = s + item + "\n";
+        }
+        return Text(s);
       },
     );
   }
