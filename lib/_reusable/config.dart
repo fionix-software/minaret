0b21decb2a112b4524a9a8f669a13c85220c32ac
Nazeb Zurati <nazeb04@gmail.com>
2020-05-12 11:39:19 +0800

    Implement FRC

--- lib/_reusable/config.dart
+++ lib/_reusable/config.dart
@@ -2,3 +2,34 @@ class Configuration {
   static const int DATABASE_VERSION = 2;
   static const String DATABASE_NAME = 'minaret';
 }
+
+/* 
+ * Version
+ * -------
+ * 20200512-1
+ *
+ * Language 
+ * --------
+ * Dart
+ * 
+ * Framework
+ * ---------
+ * Flutter
+ * 
+ * Dependencies
+ * ------------
+ * You need to add these packages into 'pubspec.yaml' file:- 
+ * - sqflite
+ * - path_provider
+ * - font_awesome_flutter
+ * - http
+ * - flutter_launcher_icons
+ * 
+ * Please get the latest package version from 'https://pub.dev/'
+ * 
+ * Instruction
+ * -----------
+ * Copy dart-flutter directory content to '_reusable' inside lib directory
+ * Linux command: 'cp -r dart-flutter/ ${project-path}/lib/; mv ${project-path}/lib/dart-flutter ${project-path}/lib/_reusable'
+ * 
+ */
